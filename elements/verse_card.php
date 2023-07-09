83a7525530532aa209913fb81e5177a365f02bb2
Jacob <jtgraham38@gmail.com>
2023-07-09 09:15:09 -0400

    added styling to the link on the verse card

--- elements/verse_card.php
+++ elements/verse_card.php
@@ -26,7 +26,7 @@ if (!defined('ABSPATH')) {
 
         <div class="verse_footer">
             <p class="verse_footer_text"></p>
-            <a href="https://github.com/jtgraham38/BibleBuddy/tree/main" target="_blank">Bible Buddy</a>
+            <a class="bb_link" href="https://github.com/jtgraham38/BibleBuddy/tree/main" target="_blank">Bible Buddy</a>
         </div>
     </div>
     <div class="hover_card_arrow" data-popper-arrow></div>
