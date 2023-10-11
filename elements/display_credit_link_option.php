b0bc361b27f8576d7e0d17984640bd231a7b633a
Jacob <jtgraham38@gmail.com>
2023-10-11 01:12:52 -0400

    Added link to admin page.

--- elements/display_credit_link_option.php
+++ elements/display_credit_link_option.php
@@ -6,3 +6,8 @@ if (!defined('ABSPATH')) {
 ?>
 
 <input type="checkbox" name="display_credit_link" <?php checked(get_option('display_credit_link')) ?> value="1"/>
+
+<br>
+<br>
+
+<p>Visit <a href="https://jacob-t-graham.com" target="_blank">my website</a> to get in touch with me!</p>
